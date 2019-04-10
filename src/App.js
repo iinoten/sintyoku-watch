729ffabfa4caa67c7fa78c8a807557b755de3ada
Iino Ten <nteten0313@icloud.com>
2019-04-10 10:18:43 +0900

    add first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, withRouter, Link } from 'react-router-dom'
+
+import TopPage from './TopPage/TopPage'
+import InductionToMobile from './InductionToMobile/InductionToMobile'
 
 class App extends Component {
+  componentDidMount() {
+    let getDevice = (() => {
+      let ua = navigator.userAgent;
+      if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+          return 'sp';
+      }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+          return 'tab';
+      }else{
+          return 'other';
+      }
+    })();
+    if( getDevice ) {
+
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div id="App">
+          <Route path='/' render={ () => <TopPage /> } />
+          <Route path='induction' render={ () => <InductionToMobile />} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
+export default  App;
